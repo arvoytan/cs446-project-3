498e11252b8dd81003885d7cd635bd56b4ebe4d5
Alexander R Voytan <arvoytan@ecc-jaguar.csuchico.edu>
2019-10-29 23:34:48 -0700

    working udp and tcp w/ flags

--- packets.c
+++ packets.c
@@ -36,26 +36,26 @@ char valid_capture(int return_value, pcap_t *pcap_handle, char use_file);
 
 int main(int argc, char *argv[]) {
 
-	pcap_t *pcap_handle = NULL;             /* Handle for PCAP library */
-	struct pcap_pkthdr *packet_hdr = NULL;  /* Packet header from PCAP */
-	const u_char *packet_data = NULL;       /* Packet data from PCAP */
-	int ret = 0;                            /* Return value from library calls */
-	char use_file = 0;                      /* Flag to use file or live capture */
+  pcap_t *pcap_handle = NULL;             /* Handle for PCAP library */
+  struct pcap_pkthdr *packet_hdr = NULL;  /* Packet header from PCAP */
+  const u_char *packet_data = NULL;       /* Packet data from PCAP */
+  int ret = 0;                            /* Return value from library calls */
+  char use_file = 0;                      /* Flag to use file or live capture */
 
-	/* Setup the capture and get the valid handle. */
-	pcap_handle = setup_capture(argc, argv, &use_file);
+  /* Setup the capture and get the valid handle. */
+  pcap_handle = setup_capture(argc, argv, &use_file);
 
-	/* Loop through all the packets in the trace file.
-	 * ret will equal -2 when the trace file ends.
-	 * ret will never equal -2 for a live capture. */
-	ret = pcap_next_ex(pcap_handle, &packet_hdr, &packet_data);
-	while( ret != -2 ) {
-		if( valid_capture(ret, pcap_handle, use_file) ){
-     /*
-      *
-      * this is the ehternet code working
-      *
-      */
+  /* Loop through all the packets in the trace file.
+   * ret will equal -2 when the trace file ends.
+   * ret will never equal -2 for a live capture. */
+  ret = pcap_next_ex(pcap_handle, &packet_hdr, &packet_data);
+  while( ret != -2 ) {
+    if( valid_capture(ret, pcap_handle, use_file) ){
+      /*
+       *
+       * this is the ehternet code working
+       *
+       */
       for(int i = 6; i<12; i++){
         printf("%01x",packet_data[i]);
         if(i<11)
@@ -80,30 +80,39 @@ int main(int argc, char *argv[]) {
           if(i < 29)
             printf(".");
         }
-       printf(" -> ");
-       for (int i = 30; i<34; i++){
-         printf("%d", packet_data[i]);
-         if(i< 33)
-           printf(".");
-       }
-       printf("\n");
-       if(packet_data[23] == 6){
-         printf("   [TCP] ");
-         int source  = (packet_data[34]<<2) +packet_data[35];
-         int dest = (packet_data[36]<<2) + packet_data[37];
-         printf("%d", source);
-         printf(" -> ");
-         printf("%d", dest);
-       }
-       else if(packet_data[23] == 17){
-         printf("   [UDP] ");
-         int source  = (packet_data[34]<<2) +packet_data[35];
-         int dest = (packet_data[36]<<2) + packet_data[37];
-         printf("%d", source);
-         printf(" -> ");
-         printf("%d", dest);
-       }
-       printf("\n");
+        printf(" -> ");
+        for (int i = 30; i<34; i++){
+          printf("%d", packet_data[i]);
+          if(i< 33)
+            printf(".");
+        }
+        printf("\n");
+        if(packet_data[23] == 6){
+          printf("   [TCP] ");
+          int source  = (packet_data[34]<<8) +packet_data[35];
+          int dest = (packet_data[36]<<8) + packet_data[37];
+          printf("%d", source);
+          printf(" -> ");
+          printf("%d", dest);
+          if(packet_data[47] == 2)
+            printf(" SYN");
+          else if(packet_data[47] == 1)
+            printf(" FIN");
+        }
+        else if(packet_data[23] == 17){
+          printf("   [UDP] ");
+          int source  = (packet_data[34]<<8) +packet_data[35];
+          int dest = (packet_data[36]<<8) + packet_data[37];
+          printf("%d", source);
+          printf(" -> ");
+          printf("%d", dest);
+        }
+        else{ 
+          printf("   [");
+          printf("%d", packet_data[23]);
+          printf("]");
+        }
+        printf("\n");
       }
       /*
        *
@@ -124,116 +133,143 @@ int main(int argc, char *argv[]) {
         printf(" -> ");
         printf("%s", ipv6_hex_dest);
         printf("\n");
+        if(packet_data[20] == 6){//ipv6 tcp
+          printf("   [TCP] ");
+          int source  = (packet_data[54]<<8) +packet_data[55];
+          int dest = (packet_data[56]<<8) + packet_data[57];
+          printf("%d", source);
+          printf(" -> ");
+          printf("%d", dest);
+          if((packet_data[67]&2) == 2)
+            printf(" SYN");
+          else if((packet_data[67]&1) == 1)
+            printf(" FIN");
+        }
+        else if(packet_data[20]==17){//ipv6 udp
+          printf("   [UDP] ");
+          int source  = (packet_data[54]<<8) +packet_data[55];
+          int dest = (packet_data[56]<<8) + packet_data[57];
+          printf("%d", source);
+          printf(" -> ");
+          printf("%d", dest);
+        }
+        /*else{
+          printf("   [");
+          printf("%d", packet_data[23]);
+          printf("]");
+        }*/
+        printf("\n");
       }
-      printf("\n");
       /*
-			 * Print the fifth byte of the packet
-			 * printf("%02X", packet_data[4]);
-			 */
-		}
+       * Print the fifth byte of the packet
+       * printf("%02X", packet_data[4]);
+       */
+    }
+    else{ //not ipv4 or ipv6
+    }
 
-		/* Get the next packet */
-		ret = pcap_next_ex(pcap_handle, &packet_hdr, &packet_data);
-	}
+    /* Get the next packet */
+    ret = pcap_next_ex(pcap_handle, &packet_hdr, &packet_data);
+  }
 
-	cleanup_capture(pcap_handle);
-	return 0;
+  cleanup_capture(pcap_handle);
+  return 0;
 }
 
 /****************************************************************************************
  * You can ignore everything below this unless you are interested.
  ***************************************************************************************/
 pcap_t* setup_capture(int argc, char *argv[], char *use_file) {
-	char *trace_file = NULL;                /* Trace file to process */
-	pcap_t *pcap_handle = NULL;             /* Handle for PCAP library to return */
-	char pcap_buff[PCAP_ERRBUF_SIZE];       /* Error buffer used by pcap functions */
-	char *dev_name = NULL;                  /* Device name for live capture */
+  char *trace_file = NULL;                /* Trace file to process */
+  pcap_t *pcap_handle = NULL;             /* Handle for PCAP library to return */
+  char pcap_buff[PCAP_ERRBUF_SIZE];       /* Error buffer used by pcap functions */
+  char *dev_name = NULL;                  /* Device name for live capture */
 
-	/* Check command line arguments */
-	if( argc > 2 ) {
-		fprintf(stderr, "Usage: %s [trace_file]\n", argv[0]);
-		exit(-1);
-	}
-	else if( argc > 1 ){
-		*use_file = 1;
-		trace_file = argv[1];
-	}
-	else {
-		*use_file = 0;
-	}
+  /* Check command line arguments */
+  if( argc > 2 ) {
+    fprintf(stderr, "Usage: %s [trace_file]\n", argv[0]);
+    exit(-1);
+  }
+  else if( argc > 1 ){
+    *use_file = 1;
+    trace_file = argv[1];
+  }
+  else {
+    *use_file = 0;
+  }
 
-	/* Open the trace file, if appropriate */
-	if( *use_file ){
-		pcap_handle = pcap_open_offline(trace_file, pcap_buff);
-		if( pcap_handle == NULL ){
-			fprintf(stderr, "Error opening trace file \"%s\": %s\n", trace_file, pcap_buff);
-			exit(-1);
-		}
-	}
-	/* Lookup and open the default device if trace file not used */
-	else{
-		dev_name = pcap_lookupdev(pcap_buff);
-		if( dev_name == NULL ){
-			fprintf(stderr, "Error finding default capture device: %s\n", pcap_buff);
-			exit(-1);
-		}
+  /* Open the trace file, if appropriate */
+  if( *use_file ){
+    pcap_handle = pcap_open_offline(trace_file, pcap_buff);
+    if( pcap_handle == NULL ){
+      fprintf(stderr, "Error opening trace file \"%s\": %s\n", trace_file, pcap_buff);
+      exit(-1);
+    }
+  }
+  /* Lookup and open the default device if trace file not used */
+  else{
+    dev_name = pcap_lookupdev(pcap_buff);
+    if( dev_name == NULL ){
+      fprintf(stderr, "Error finding default capture device: %s\n", pcap_buff);
+      exit(-1);
+    }
 
-		/* Use buffer length as indication of warning, per pcap_open_live(3). */
-		pcap_buff[0] = 0;
+    /* Use buffer length as indication of warning, per pcap_open_live(3). */
+    pcap_buff[0] = 0;
 
-		pcap_handle = pcap_open_live(dev_name, BUFSIZ, 1, MAX_BUFFER_TIME_MS, pcap_buff);
-		if( pcap_handle == NULL ){
-			fprintf(stderr, "Error opening capture device %s: %s\n", dev_name, pcap_buff);
-			exit(-1);
-		}
-		if( pcap_buff[0] != 0 ) {
-			printf("Warning: %s\n", pcap_buff);
-		}
+    pcap_handle = pcap_open_live(dev_name, BUFSIZ, 1, MAX_BUFFER_TIME_MS, pcap_buff);
+    if( pcap_handle == NULL ){
+      fprintf(stderr, "Error opening capture device %s: %s\n", dev_name, pcap_buff);
+      exit(-1);
+    }
+    if( pcap_buff[0] != 0 ) {
+      printf("Warning: %s\n", pcap_buff);
+    }
 
-		printf("Capturing on interface '%s'\n", dev_name);
-	}
+    printf("Capturing on interface '%s'\n", dev_name);
+  }
 
-	return pcap_handle;
+  return pcap_handle;
 
 }
 
 void cleanup_capture(pcap_t *handle) {
-	/* Close the trace file or device */
-	pcap_close(handle);
+  /* Close the trace file or device */
+  pcap_close(handle);
 }
 
 char valid_capture(int return_value, pcap_t *pcap_handle, char use_file) {
-	static int idle_count = 0;  /* Count of idle periods with no packets */
-	char ret = 0;               /* Return value, invalid by default */
+  static int idle_count = 0;  /* Count of idle periods with no packets */
+  char ret = 0;               /* Return value, invalid by default */
 
-	/* A general error occurred */
-	if( return_value == -1 ) {
-		char err_str[] = "Error processing packet:";
-		pcap_perror(pcap_handle, err_str);
-		cleanup_capture(pcap_handle);
-		exit(-1);
-	}
+  /* A general error occurred */
+  if( return_value == -1 ) {
+    char err_str[] = "Error processing packet:";
+    pcap_perror(pcap_handle, err_str);
+    cleanup_capture(pcap_handle);
+    exit(-1);
+  }
 
-	/* Timeout occured for a live packet capture */
-	else if( (return_value == 0) && (use_file == 0) ){
-		if( ++idle_count >= MAX_IDLE_TIME ){
-			printf("Timeout waiting for additional packets on interface\n");
-			cleanup_capture(pcap_handle);
-			exit(0);
-		}
-	}
+  /* Timeout occured for a live packet capture */
+  else if( (return_value == 0) && (use_file == 0) ){
+    if( ++idle_count >= MAX_IDLE_TIME ){
+      printf("Timeout waiting for additional packets on interface\n");
+      cleanup_capture(pcap_handle);
+      exit(0);
+    }
+  }
 
-	/* Unexpected/unknown return value */
-	else if( return_value != 1 ) {
-		fprintf(stderr, "Unexpected return value (%i) from pcap_next_ex()\n", return_value);
-		cleanup_capture(pcap_handle);
-		exit(-1);
-	}
-	/* Normal operation, packet arrived */
-	else{
-		idle_count = 0;
-		ret = 1;
-	}
+  /* Unexpected/unknown return value */
+  else if( return_value != 1 ) {
+    fprintf(stderr, "Unexpected return value (%i) from pcap_next_ex()\n", return_value);
+    cleanup_capture(pcap_handle);
+    exit(-1);
+  }
+  /* Normal operation, packet arrived */
+  else{
+    idle_count = 0;
+    ret = 1;
+  }
 
-	return ret;
+  return ret;
 }
